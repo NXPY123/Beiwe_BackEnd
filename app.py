ed17a1a23598fa3851aa3fa6af1f5f1cbcc3518a
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2022-06-26 08:51:28 +0530

    Cleaned up code

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ from google.protobuf.json_format import MessageToJson
 from google.protobuf.json_format import MessageToDict
 from google.cloud.vision import AnnotateFileRequest
 import proto
+from labels import label
 
 app = Flask(__name__)
 
 
 credentials = service_account.Credentials.from_service_account_file('./config/beive-354409-0e474f6066a3.json')
-image = vision_v1.types.Image()
+
 client = vision_v1.ImageAnnotatorClient(credentials=credentials)
 
 
@@ -26,24 +27,15 @@ def hello():
     return 'Hello, World!'
 
 @app.route('/website',endpoint = 'label', methods=['GET', 'POST'])
-def label():
+def web_request():
 
     img_uri = request.args.get('params') 
-   
-   
-    image.source.image_uri = img_uri
-    response = client.label_detection(image=image)
-    
 
-    #response = MessageToJson(response, preserving_proto_field_name = True)
-    #desired_res 
-    #labels = response.label_annotations
+    response = label(img_uri,client) #Get response as dict
+    return response
     
-    serializable_tags = [proto.Message.to_dict(tag) for tag in response.label_annotations]
     
-    print(serializable_tags)
     
-    return {'serial':serializable_tags}
     #return render_template('image_label.html',labels=labels)
 
 
---
+++ labels.py
@@ -0,0 +1,32 @@
+from flask import Flask,request
+import io
+import os
+import requests
+import json
+# Imports the Google Cloud client library
+from google.cloud import vision_v1
+from google.oauth2 import service_account
+from google.protobuf.json_format import MessageToDict
+from google.cloud.vision import AnnotateFileRequest
+import proto
+
+
+
+def label(img_uri,client):
+
+    
+
+    image = vision_v1.types.Image() #Image Object
+   
+    image.source.image_uri = img_uri #To get image from img_link
+    response = client.label_detection(image=image) #Detect Labels
+    
+
+   
+    
+    serializable_tags = [proto.Message.to_dict(tag) for tag in response.label_annotations] #Convert object to list type
+    
+    
+    
+    return {'serial':serializable_tags}
+    #return render_template('image_label.html',labels=labels)
