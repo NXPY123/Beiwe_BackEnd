bd3013af60c4b54b22309fa266007f4bdb772ea3
Neeraj P Yetheendran <46917698+NXPY123@users.noreply.github.com>
2022-06-29 22:51:57 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -46,11 +46,11 @@ def create_app():
    
    
     #Register auth blueprint
-    from .auth import auth as auth_blueprint
+    from auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     #Register main blueprint
-    from .main import main as main_blueprint
+    from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
