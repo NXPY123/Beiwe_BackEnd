517ad5cae16b2a351b30773574bbdfbfba81001d
Neeraj P Yetheendran <46917698+NXPY123@users.noreply.github.com>
2022-07-17 08:39:52 +0530

    Delete database directory

--- database/db_create.py
+++
@@ -1,8 +0,0 @@
-from venv import create
-from __init__ import db, create_app
-from models import User
-from flask_migrate import Migrate
-
-#db.create_all(app=create_app())
-
-migrate = Migrate(app=create_app(),db=db)
