7a0fc4ca5b300a6948bc6793f5c6738f623ff6c4
Neeraj P Yetheendran <46917698+NXPY123@users.noreply.github.com>
2022-09-28 10:11:42 +0000

    path_dir fixed

--- code/path_dir.py
+++ code/path_dir.py
@@ -2,13 +2,13 @@ import subprocess
 
 repo_dir = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8')
 extension_py = repo_dir+"/code/extension.py"
-main_py = repo_dir+"/code/main.py"
-labels_py = repo_dir+"/code/labels.py"
-auth_py = repo_dir+"/code/auth.py"
-app_py = repo_dir+"/code/app.py"
-init_py = repo_dir+"/code/__init__.py"
-absent_labels_csv = repo_dir+"/code/csv/absent_labels.csv"
-google_labels_csv = repo_dir+"/code/csv/google_labels.csv"
-labels_possible_suggested_csv = repo_dir+"/code/csv/labels_possible_suggested.csv"
+main_py = repo_dir+"./code/main.py"
+labels_py = repo_dir+"./code/labels.py"
+auth_py = repo_dir+"./code/auth.py"
+app_py = repo_dir+"./code/app.py"
+init_py = repo_dir+"./code/__init__.py"
+absent_labels_csv = repo_dir+"./code/csv/absent_labels.csv"
+google_labels_csv = repo_dir+"./code/csv/google_labels.csv"
+labels_possible_suggested_csv = repo_dir+"./code/csv/labels_possible_suggested.csv"
 
-
+print(repo_dir)
