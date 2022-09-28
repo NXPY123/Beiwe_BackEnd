69b1f535cd9e6b6d73588e5b0095ce95c3c5b695
Neeraj P Yetheendran <46917698+NXPY123@users.noreply.github.com>
2022-09-28 10:16:59 +0000

    path_dir not functioning as expected

--- code/path_dir.py
+++ code/path_dir.py
@@ -1,14 +1,14 @@
 import subprocess
 
 repo_dir = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8')
-extension_py = repo_dir+"/code/extension.py"
-main_py = repo_dir+"./code/main.py"
-labels_py = repo_dir+"./code/labels.py"
-auth_py = repo_dir+"./code/auth.py"
-app_py = repo_dir+"./code/app.py"
-init_py = repo_dir+"./code/__init__.py"
-absent_labels_csv = repo_dir+"./code/csv/absent_labels.csv"
+extension_py = "extension.py"
+main_py = "main.py"
+labels_py = "labels.py"
+auth_py = "auth.py"
+app_py = "app.py"
+init_py = "__init__.py"
+absent_labels_csv = "csv/absent_labels.csv"
 google_labels_csv = "csv/google_labels.csv" #repo_dir+"./code/csv/google_labels.csv"
-labels_possible_suggested_csv = repo_dir+"./code/csv/labels_possible_suggested.csv"
+labels_possible_suggested_csv = "csv/labels_possible_suggested.csv"
 
 print(repo_dir)
